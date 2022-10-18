02e620fb5b149d92f8e3680949bba0a6aebdd4b4
BuildTools <unconfigured@null.spigotmc.org>
2022-10-18 22:35:45 +0200

    asd

---
+++ scripts/logged-in.js
@@ -0,0 +1,7 @@
+function loggedIn() {
+    //ak uzivatel nie je prihlaseny daj ho do login.html
+    if (false) {
+        let x = document.getElementById("reservation")
+        x.setAttribute("href", "login.html")
+    }
+}
